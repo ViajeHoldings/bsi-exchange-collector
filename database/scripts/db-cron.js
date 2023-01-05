cf0adb5c6114e0482365d207a828fca4df01cd7f
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2023-01-05 16:37:13 +0700

    ✨ Add old data deletion cron
    Changes:
    - Delete data older than 3 days ago.
    - Add cron to execute deletion every Sunday night.

---
+++ database/scripts/db-cron.js
@@ -0,0 +1,15 @@
+const cron = require('node-cron');
+
+const mongoConnect = require('./mongo-connect.js');
+const deleteOldData = require('./delete-old-data.js');
+
+const mongoUrl =
+  process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/BSI-exchange-collector';
+
+mongoConnect(mongoUrl);
+
+/* ======================= Cron Jobs ======================= */
+
+const everySundayNight = '0 0 * * 0';
+
+cron.schedule(everySundayNight, deleteOldData);
